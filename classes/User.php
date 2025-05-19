6d6e1bd68ed9681c26eec48b9ef9b136103819f8
BAKARAbdellatif <abakr@email.com>
2025-05-19 19:39:17 +0100

    add classes

---
+++ classes/User.php
@@ -0,0 +1,59 @@
+<?php
+
+class User
+{
+    private $id;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $password;
+
+    public function __construct($id = null, $nom = null, $prenom = null, $email = null, $password = null)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNom()
+    {
+        $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom()
+    {
+        $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getEmail()
+    {
+        $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
